be48170b71a9f0de13fecb9b065aeec13768b8a8
SapphicBear <sleepyscreenart@gmail.com>
2026-03-20 22:57:23 +0100

    change port

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const path = require("node:path");
 const app = express();
 const indexRouter = require("./routes/indexRouter");
 const newRouter = require("./routes/newRouter");
+const PORT = 8080;
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
@@ -13,7 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/", indexRouter);
 app.use("/new", newRouter);
 
-// TODO: add 404 view
+
 app.use((req, res, next) => {
     res.status(404).send("<p>404 page not found! Sorry!</p>");
 })
@@ -22,9 +23,9 @@ app.use((err, req, res, next) => {
     res.status(err.statusCode || 500).send(err.message);
 });
 
-app.listen(3000, (err) => {
+app.listen(PORT, (err) => {
     if (err) {
         throw err;
     }
-    console.log(`Live on localhost:3000`)
+    console.log(`Live ${PORT}`);
 });
