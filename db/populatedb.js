d37bf391f043ba190d6cc22763d567659dc18a41
SapphicBear <sleepyscreenart@gmail.com>
2026-03-23 18:54:36 +0100

    Start implementing a database for messages
    Install dependencies for postgresql

---
+++ db/populatedb.js
@@ -0,0 +1,29 @@
+#! /usr/bin/env node
+const { argv } = require("node:process");
+const { Client } = require("pg");
+
+const SQL = `
+CREATE TABLE IF NOT EXISTS messages (
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    username VARCHAR (255),
+    message VARCHAR (255),
+    date VARCHAR (255)
+);
+
+INSERT INTO messages (username, message, date)
+VALUES
+('Tay', 'Hope everyone is doing good!', ${Date.now()});
+`;
+
+async function main(arg) {
+    console.log("Seeding Database");
+    const client = new Client({
+        connectionString: arg,
+    });
+    await client.connect();
+    await client.query(SQL);
+    await client.end();
+    console.log("Done");
+}
+
+main(argv[2]);
---
+++ db/queries.js
@@ -0,0 +1,15 @@
+const pool = require("./pool");
+
+async function getAllMessages() {
+    const { rows } = await pool.query("SELECT * FROM messages;");
+    return rows;
+}
+
+async function addNewMessage(username, message, date = Date.now()) {
+    await pool.query("INSERT INTO messages (username, message, date) VALUES ($1, $2, $3)", [username, message, date]);
+}
+
+module.exports = {
+    getAllMessages,
+    addNewMessage
+};
