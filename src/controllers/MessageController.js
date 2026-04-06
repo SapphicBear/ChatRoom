2ac574fc50e9fda6d7546cee823135ef3057dd86
SapphicBear <sleepyscreenart@gmail.com>
2026-04-06 09:59:35 +0200

    Bug fix: Error handling works
    Errors made on the new message page now will show up correctly and not
    nuke user information.

--- src/controllers/MessageController.js
+++ src/controllers/MessageController.js
@@ -1,5 +1,4 @@
 const links = require("./../routes/links");
-const pages = require("./../routes/pages")[1];
 const db = require("./../../db/queries");
 const { body, validationResult, matchedData} = require("express-validator");
 
@@ -7,28 +6,32 @@ const { body, validationResult, matchedData} = require("express-validator");
 const errors = {
     alphaNum: "Must only contain letters and numbers",
     lengthUser: "Must be between 1 and 20 characters long",
-    lengthBody: "Must be between 1 and 255 characters long",
+    lengthBody: "Must be between 1 and 500 characters long",
 };
 const validateUser = [
-    body("username").trim()
+    body("username").trim().escape()
         .isAlphanumeric().withMessage(`username: ${errors.alphaNum}`)
         .isLength({ min: 1, max: 20}).withMessage(`username: ${errors.lengthUser}`),
-    body("message").trim()
-        .isLength({ min: 1, max: 255 }).withMessage(`message body: ${errors.lengthBody}`),
+    body("message").trim().escape()
+        .isLength({ min: 1, max: 500 }).withMessage(`message body: ${errors.lengthBody}`),
 ];
 
 const usersCreatePost = [
     validateUser,
     async (req, res) => {
         const errors = validationResult(req);
+        const {username} = matchedData(req);
+        const message = req.body.message; 
         if (!errors.isEmpty()) {
             return res.status(400).render("new.ejs", {
                 links: links,
                 pages: "New",
                 errors: errors.array(),
+                username: username,
+                message: message,
             });
         }
-        const {username, message} = matchedData(req);
+        
         await db.addNewMessage(username, message);
         console.log("Added to database");
         res.redirect("/");
@@ -36,7 +39,7 @@ const usersCreatePost = [
 ];
 
 async function newMessageGet(req, res) {
-    res.render("new", { links: links, pages: "New"});
+    res.render("new", { links: links, pages: "New", username: "", message: ""});
 }
 
 async function messagesGet(req, res) {
