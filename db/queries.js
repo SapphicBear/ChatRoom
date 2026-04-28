818deef70187c8a1e5eb8a2cd55372d17dffaa0c
SapphicBear <sleepyscreenart@gmail.com>
2026-04-28 21:15:54 +0200

    Rework db message queries into class

--- db/queries.js
+++ db/queries.js
@@ -10,7 +10,19 @@ async function addNewMessage(username, message, date = format(Date.now(), "HH:mm
     await pool.query("INSERT INTO messages (username, message, date) VALUES ($1, $2, $3)", [username, message, date]);
 }
 
+class Message {
+    
+    async getAllMessages() {
+        const { rows } = await pool.query("SELECT * FROM messages;");
+        return rows;
+    }
+
+    async addMessage(username, message, date = format(Date.now(), "HH:mm - PPP")) {
+        await pool.query("INSERT INTO messages (username, message, date) VALUES ($1, $2, $3);", [username, message, date]);
+    }
+
+}
+
 module.exports = {
-    getAllMessages,
-    addNewMessage
+    Message
 };
